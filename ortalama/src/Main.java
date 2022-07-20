a87640b12100854de23f4f24060b4debe91c4005
Betul Bircan <bircan.betul_1996@hotmail.com>
2022-07-20 22:22:36 +0300

    değişkenler tanımlandı ve Scanner Sınıfı Oluşturuldu

--- ortalama/src/Main.java
+++ ortalama/src/Main.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	 /*Java ile Matematik, Fizik, Kimya, Türkçe, Tarih, Müzik derslerinin sınav puanlarını kullanıcıdan alan ve
+        ortalamalarını hesaplayıp ekrana bastırılan programı yazın.
+         */
+
+        // Değişkenleri Oluştur.
+        int mat, fizik, kimya, turkce, tarih, muzik;
+
+        //Scanner sınıfı tanımlama
+        Scanner inp = new Scanner(System.in);
     }
 }
