fd40b4d780e920e8089cc244b08f20e5f0826af7
Betul Bircan <bircan.betul_1996@hotmail.com>
2022-07-20 22:23:38 +0300

    Kullanıcıdan değerler alındı

--- ortalama/src/Main.java
+++ ortalama/src/Main.java
@@ -12,5 +12,24 @@ public class Main {
 
         //Scanner sınıfı tanımlama
         Scanner inp = new Scanner(System.in);
+
+        //Kullanıcıdan değerleri alma
+        System.out.print("Matematik Notunuz: ");
+        mat = inp.nextInt();
+
+        System.out.print("Fizik Notunuz: ");
+        fizik = inp.nextInt();
+
+        System.out.print("Kimya Notunuz: ");
+        kimya = inp.nextInt();
+
+        System.out.print("Türkçe Notunuz: ");
+        turkce = inp.nextInt();
+
+        System.out.print("Tarih Notunuz: ");
+        tarih = inp.nextInt();
+
+        System.out.print("Müzik Notunuz: ");
+        muzik = inp.nextInt();
     }
 }
