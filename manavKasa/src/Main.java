81c66627ab1269fcaa1b88c658e5772b14957a79
Betul Bircan <bircan.betul_1996@hotmail.com>
2022-07-23 02:51:25 +0300

    değişkenler tanımlandı

---
+++ manavKasa/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+	/*
+	Java ile kullanıcıların manavdan almış oldukları ürünlerin kilogram değerlerine göre toplam tutarını ekrana yazdıran programı yazın.
+
+    Meyveler ve KG Fiyatları
+
+    Armut : 2,14 TL
+    Elma : 3,67 TL
+    Domates : 1,11 TL
+    Muz: 0,95 TL
+    Patlıcan : 5,00 TL
+	 */
+
+
+        //Değişkenleri Tanımlama
+        double armutKilo, elmaKilo, domatesKilo, muzKilo, patlicanKilo,
+                armut = 2.14, elma = 3.67, domates = 1.11, muz = 0.95, patlican = 5.00,  total;
+    }
+}
