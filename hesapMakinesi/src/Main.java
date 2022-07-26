9d050dbed850bdfc41b2c6df3ccd86eece20f691
Betul Bircan <bircan.betul_1996@hotmail.com>
2022-07-27 01:52:09 +0300

    switchcase kullanılarak dört işlem yapıldı

--- hesapMakinesi/src/Main.java
+++ hesapMakinesi/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	 Hesap makinesini switch-case kullanarak yapınız.
 	 */
 
-        //DEĞİŞKENLERİ TANIMLAMA VE KULLANICIDAN BİLGİLERİ ALMA
+        //değişkenleri tanımlama ve kullanıcıdan bilgileri alma
         int number1, number2, select;
 
         Scanner input = new Scanner(System.in);
@@ -20,6 +20,33 @@ public class Main {
         System.out.println("Birini Seçiniz: ");
         System.out.println("1-Toplama\n2-Çıkarma\n3-Çarpma\n4-Bölme");
         select = input.nextInt();
+
+        //switchcase ile 4 işlemli hesap makinesi işlemlerini yapma
+        switch (select) {
+            case 1:
+                int toplam = number1 + number2;
+                System.out.println("Toplam : " + toplam);
+                break;
+            case 2:
+                int cikarma= number1 - number2;
+                System.out.println("Çıkarma : " + cikarma);
+                break;
+            case 3:
+                int carpma = number1 * number2;
+                System.out.println("Çarpma : " + carpma);
+                break;
+            case 4:
+                if(number2 != 0) {
+                    int bolme = number1 / number2;
+                    System.out.println("Bölme: " + bolme);
+                }
+                else {
+                    System.out.println("0 a bölünemez!!");
+                }
+                break;
+            default:
+                System.out.println("Yanlış Seçim Yaptınız.Tekrar deneyin...");
+        }
 
     }
 }
